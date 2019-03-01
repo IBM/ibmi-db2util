a1fc5d2afdaef46c74121a80ee29c369925c4195
Kevin Adler <kadler@us.ibm.com>
2019-02-28 23:17:53 -0600

    Remove unneeded code

--- cpysqlincludes.sh
+++
@@ -1,9 +0,0 @@
-#!/QOpenSys/usr/bin/ksh
-ls /QIBM/include/sql* > ./list2
-for i in $(< ./list2)
-do
-  echo "PREPARATION COPY"
-  system -v "CPY OBJ('$i') TODIR('/usr/include/') TOCCSID(*STDASCII) DTAFMT(*TEXT) REPLACE(*YES)"                                      
-done
-
-
